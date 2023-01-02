bc7b39f68aae7432c41a41db594435bc7788088b
mohamed mostfa <mohamed.mostfa1970@gmail.com>
2023-01-03 01:32:32 +0200

    Mohamed Mostfa - SaveBtn function in Customer page

--- Mobile_Repairs_M_S/Customers.cs
+++ Mobile_Repairs_M_S/Customers.cs
@@ -32,5 +32,35 @@ namespace Mobile_Repairs_M_S
         {
 
         }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (CustNameTb.Text == "")
+                {
+                    MessageBox.Show("Missing Data");
+                }
+                else
+                {
+                    string CName = CustNameTb.Text;
+                    string CPhone = CustPhoneTb.Text;
+                    string CAdd = CustAddTb.Text;
+                    string Query = "insert into CustomerTbl values('{0}','{1}','{2}')";
+                    Query = string.Format(Query, CName,CPhone,CAdd);
+                    Con.SetData(Query);
+                    ShowCustomersList();
+                    MessageBox.Show("Customer Added");
+                    CustNameTb.Text = "";
+                    CustPhoneTb.Text = "";
+                    CustAddTb.Text = "";
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
     }
 }
