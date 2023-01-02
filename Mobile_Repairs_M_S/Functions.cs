0853805a7c8f306c14ffb0933a0163f7c7646b56
mohamed mostfa <mohamed.mostfa1970@gmail.com>
2023-01-03 00:57:58 +0200

    Mohamed Mostfa - connecting to the database

--- Mobile_Repairs_M_S/Functions.cs
+++ Mobile_Repairs_M_S/Functions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,19 @@ namespace Mobile_Repairs_M_S
 {
     class Functions
     {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string ConStr;
+
+        public Functions()
+        {
+            ConStr = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\fager\OneDrive\المستندات\MobileRepairDB.mdf; Integrated Security = True; Connect Timeout = 30";
+            Con = new SqlConnection(ConStr);
+            Cmd = new SqlCommand();
+            Cmd.Connection = Con;
+        }
+
     }
 }
